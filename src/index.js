9c2b127a52056def9d7efacb269623d6000c660b
lac <laclys@126.com>
2017-12-10 16:00:51 +0800

    添加redux调试工具配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 
 import App  from './App'
 import {counter, addCount, minuxCount, addCountAsync} from './index.redux'
 
-const store = createStore(counter, applyMiddleware(thunk))
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => {}
+
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  reduxDevtools
+))
 
 function render() {
   ReactDom.render(<App store={store} addCount={addCount} minuxCount={minuxCount} addCountAsync={addCountAsync} />,document.getElementById('root'))
