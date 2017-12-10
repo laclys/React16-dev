2da9953b8f56b4f10d0401bb9369c885341cc6f4
lac <laclys@126.com>
2017-12-10 15:16:00 +0800

    redux和react组件解耦

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {addCount} from './index.redux'
 
 class App extends React.Component {
   render() {
     const store = this.props.store
     const num = store.getState()
+    const addCount = this.props.addCount
+    const minuxCount = this.props.minuxCount
     return (
       <div>
         <h1>Show:{num}</h1>
         <button onClick={() => store.dispatch(addCount())} >ADD 1</button>
+        <button onClick={() => store.dispatch(minuxCount())} >MINUS 1</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDom from 'react-dom'
 import {createStore} from 'redux'
 
 import App  from './App'
-import {counter} from './index.redux'
+import {counter, addCount, minuxCount} from './index.redux'
 
 const store = createStore(counter)
 
 function render() {
-  ReactDom.render(<App store={store} />,document.getElementById('root'))
+  ReactDom.render(<App store={store} addCount={addCount} minuxCount={minuxCount} />,document.getElementById('root'))
 }
 render()
 
