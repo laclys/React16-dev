31300953ec23640e8abdad27e7a539a8c3a176d3
lac <laclys@126.com>
2017-12-17 19:08:12 +0800

    cookie存储登录数据

--- server/user.js
+++ server/user.js
@@ -4,6 +4,10 @@ const utils = require('utility')
 const model = require('./model')
 const User = model.getModel('user')
 
+const _filter = {
+  'psd': 0,
+  '__v': 0
+}
 
 Router.get('/list',function (req, res) {
   // User.remove({},function (e, d) {})
@@ -14,13 +18,14 @@ Router.get('/list',function (req, res) {
 
 Router.post('/login', function (req, res) {
   const {user, psd} =req.body
-  User.findOne({user, psd: md5Pwd(psd)}, {'psd': 0}, function (err, doc) {
+  User.findOne({user, psd: md5Pwd(psd)}, _filter, function (err, doc) {
     if (!doc) {
       return res.json({
         code: 1,
         msg: '用户名或者密码错误'
       })
     }
+    res.cookie('userid',doc._id)
     return res.json({
       code: 0,
       data: doc
@@ -44,9 +49,27 @@ Router.post('/register', function (req, res) {
 })
 
 Router.get('/info', function (req, res) {
+  const {userid} = req.cookies
   // 用户有没有cookie
-  return res.json({
-    code: 1
+  if (!userid) {
+    return res.json({
+      code: 1
+    })
+  }
+  User.findOne({_id: userid}, _filter, function (err, doc) {
+    console.log(err)
+    if (err) {
+      return res.json({
+        code:1,
+        msg: '后端出错'
+      })
+    }
+    if (doc) {
+      return res.json({
+        code: 0,
+        data: doc
+      })
+    }
   })
 })
 
