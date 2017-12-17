f7d5d2093bcbc54a55c65ede0bf5db5d510c5edb
lac <laclys@126.com>
2017-12-17 13:33:22 +0800

    add

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,10 +1,12 @@
 
 import axios from 'axios'
+import {getRedirectPath} from '../utils'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
 
 const initState = {
+  redirectTo: '',
   isAuth: false,
   msg: '',
   user: '',
@@ -17,7 +19,7 @@ export function user (state = initState, action) {
 
   switch (action.type) {
     case REGISTER_SUCCESS:
-      return {...state, msg: '', isAuth: true, ...action.payload}
+      return {...state, msg: '',redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
     case ERROR_MSG:
       return {...state, isAuth: false, msg: action.msg}
     default: 
