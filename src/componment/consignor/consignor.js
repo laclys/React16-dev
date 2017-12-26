c219f279f0f3b9e02be6d8bf47930f169e5f49eb
lac <laclys@126.com>
2017-12-26 22:00:54 +0800

    拆分userlist

--- src/componment/consignor/consignor.js
+++ src/componment/consignor/consignor.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import axios from 'axios'
-import {Card, WhiteSpace, WingBlank} from 'antd-mobile'
 import {connect} from 'react-redux'
 import {getUserList} from '../../redux/chatuser.redux'
-
+import UserCard from '../../componment/usercard/usercard'
 
 @connect(
   state=>state.chatuser,
@@ -16,32 +14,7 @@ class Consignor extends React.Component{
   }
   render () {
     console.log(this.state)
-    return (
-      <WingBlank>
-        <WhiteSpace></WhiteSpace>
-        {this.props.userlist.map(v => {
-          return (
-            v.avatar
-              ? <Card key={v._id}>
-                <Card.Header
-                  title={v.user}
-                  // thumb={require(`../img/${v.avatar.slice(2)}.jpg`)}
-                  thumb={<img style={{width: 50}} src={require(`../img/${v.avatar.slice(2)}.jpg`)} alt=''/>}
-                  extra={<span>{v.title}</span>}
-                >
-                </Card.Header>
-                <Card.Body>
-                  {v.desc.split('\n').map(v => {
-                    return (
-                      <div key={v}>{v}</div>
-                    )
-                  })}
-                </Card.Body>
-              </Card>: null
-          )
-        })}
-      </WingBlank>
-    )
+    return <UserCard  userlist={this.props.userlist}/>
   }
 }
 
---
+++ src/componment/usercard/usercard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Card, WhiteSpace, WingBlank} from 'antd-mobile'
+
+class UserCard extends React.Component {
+  static PropTypes = {
+    userlist: PropTypes.array.isRequired
+  }
+  render () {
+    return (
+      <WingBlank>
+        <WhiteSpace></WhiteSpace>
+        {this.props.userlist.map(v => {
+          return (
+            v.avatar
+              ? <Card key={v._id}>
+                <Card.Header
+                  title={v.user}
+                  // thumb={require(`../img/${v.avatar.slice(2)}.jpg`)}
+                  thumb={<img style={{width: 50}} src={require(`../img/${v.avatar.slice(2)}.jpg`)} alt=''/>}
+                  extra={<span>{v.title}</span>}
+                >
+                </Card.Header>
+                <Card.Body>
+                  {v.type =='consignor' ? <div>委托人：{v.company}</div> : null}
+                  {v.desc.split('\n').map(d => {
+                    return (
+                      <div key={d}>{d}</div>
+                    )
+                  })}
+                  {v.type =='consignor' ? <div>报酬：{v.money}</div> : null}
+                </Card.Body>
+              </Card>: null
+          )
+        })}
+    </WingBlank>
+    )
+  }
+}
+
+export default UserCard
